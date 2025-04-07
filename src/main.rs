f386f3279ba22c03b8dceb67ab524854595385ae
Segfaulter <72930656+brudnevskij@users.noreply.github.com>
2025-04-07 01:10:10 +0000

    Added main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 mod lexer;
+mod parser;
 
 fn main() {
-    println!("Hello, world!");
+    let input = "let x = 1 + 2 * (3 + 4); croak x;";
+    let mut lexer = lexer::Lexer::new(input);
+    let tokens = lexer.parse();
+
+    let mut parser = parser::Parser::new(tokens);
+    let statements = parser.parse();
+    println!("{:#?}", statements);
 }
